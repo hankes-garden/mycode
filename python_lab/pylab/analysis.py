fad6f10107829f64a375ab3feb7a42f296d98956
jason <jason>
2014-04-09 12:57:55 +0800

    1. add anlysis-in-one for local apps 2. add localApp resutl of  20130909

--- python_lab/pylab/analysis.py
+++ python_lab/pylab/analysis.py
@@ -6,7 +6,39 @@ Created on 2014年4月4日
 
 Main entry for all analysis in one
 '''
+from data_loader import *
+from local_app import *
+from common_function import *
 
 import sys
 if __name__ == '__main__':
-    pass
+    
+    # input checking
+    if (len(sys.argv) != 4):
+        raise StandardError("Usage: %run analysis.py serialized_path cell_loc_dict_path outdir")
+    
+    # load data
+    print("Start to deserialize from file...") 
+    dcPaths = deserializeFromFile(sys.argv[1])
+     
+    print("Start to aggregate data by m_nDownBytes...")
+    dcAggregated = aggregateData(dcPaths)
+    
+    print("Start to get user number...")
+    dcAppUserNum = getAppUserNum(dcPaths)
+     
+    print("Start to clean data...")
+    cleanData(dcAggregated, dcAppUserNum)
+    
+    print("Start to construct cell-location dict...")
+    dcCellLocDict = constructCellLocDict(sys.argv[2])
+    
+    dfAgg = pd.DataFrame(dcAggregated)
+    print("data_loader is ready----")
+    
+    # local App
+    print("Start to analyse local apps...")
+    dcLocalApp = localApp(dfAgg, dcCellLocDict, True)
+    print("-->Total %d apps, %d are local." % (len(dfAgg.index), len(dcLocalApp)) )
+    
+    print("Start to output local apps...")
--- python_lab/pylab/simple_test.py
+++ python_lab/pylab/simple_test.py
@@ -6,15 +6,7 @@ Created on 2014年1月14日
 '''
 from common_function import *
 
-def outputLocalAppLocation(dcLocalApp, strOutPath):
-    with open(strOutPath, 'w') as hOutFile:
-        hOutFile.write("serviceType, lac-cid, lat, long, attributes\n")
-        for nServiceType in dcLocalApp.keys():
-            lsTopCells = dcLocalApp.get(nServiceType)
-            for tp in lsTopCells:
-                strLine = "%d, %s,%.6f,%.6f, %d\n" % (nServiceType, tp[0], tp[1][0], tp[1][1], tp[2])
-                hOutFile.write(strLine)
-    print("finished")
+
         
 
 if __name__ == '__main__':
