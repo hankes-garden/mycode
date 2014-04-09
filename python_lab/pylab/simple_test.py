00ee28c21dec803e2653d59b02002e7df94ffcb2
jason@think <hankes_garden@163.com>
2014-04-09 16:07:10 +0800

    1. add function to output cells whose location is missing;

--- python_lab/pylab/simple_test.py
+++ python_lab/pylab/simple_test.py
@@ -5,6 +5,15 @@ Created on 2014年1月14日
 @author: jason
 '''
 from common_function import *
+import pandas as pd
+
+def outputMissingLocation(dfCellLocation, strOutPath):
+    with open(strOutPath, 'w') as hOutFile:
+        for it in dfCellLocation.itertuples():
+            if pd.isnull(it[1]):
+                strLac = it[0].split('-')[0].strip()
+                strCID = it[0].split('-')[1].strip()
+                hOutFile.write("%s,%s,20120403,07:37:02\n" % (strLac, strCID) )
 
       
 if __name__ == '__main__':
