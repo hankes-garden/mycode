749124453e2ba41d2cd5d165f5e83d26d4c70f90
jason <jason>
2014-04-11 19:35:04 +0800

    1. add parallel version of role assignment; 2. add poi_role result;

--- python_lab/pylab/role_assigner.py
+++ python_lab/pylab/role_assigner.py
@@ -8,6 +8,8 @@ Created on 2014年4月10日
 from common_function import *
 
 import pandas as pd
+import multiprocessing
+import math
 
 DC_ROLE_TRANSPORTATION = dict.fromkeys(range(21, 39))
 
@@ -18,8 +20,8 @@ DC_ROLE_EDU = dict.fromkeys([52,53,54,55,56,57])
 DC_ROLE_WORK = dict.fromkeys([45,59,60,61,62,63,263,265,266,657,658,661,663,664,668,670])
 DC_ROLE_WORK.update(dict.fromkeys(range(608,647)))
 
-DC_ROLE_ENTERTAINMENT = dict.fromkeys(range(64,149))
-DC_ROLE_ENTERTAINMENT.update(dict.fromkeys(range(169,258)))
+#DC_ROLE_ENTERTAINMENT = dict.fromkeys(range(64,149))
+DC_ROLE_ENTERTAINMENT = dict.fromkeys(range(169,258))
 DC_ROLE_ENTERTAINMENT.update(dict.fromkeys([268,269,603,604,605,607,648,649,650,651,652,653,655]))
 
 ID_ROLE_UNKNOWN = 0
@@ -30,6 +32,12 @@ ID_ROLE_WORK = 4
 ID_ROLE_ENTERTAINMENT = 5
 
 DEFAULT_ROLE_COVERAGE = 2000 # unit = meter
+
+g_dfCellLocRole = pd.DataFrame()
+g_nCellNum = 0
+
+MAX_PROC_NUM = 30
+MAX_CELL_PER_PROC = 1000
 
 def generatePoiRole(strPOIPath, strOutPath):
     '''
@@ -56,8 +64,15 @@ def generatePoiRole(strPOIPath, strOutPath):
     
     dfpoi['roleID'] = lsRoleID
     dfpoi.to_csv(strOutPath)      
-          
-    print("generatePoiRole is finished.")
+    
+ 
+    print("#unknonw POI = %d " % (len(dfpoi[dfpoi['roleID'] == 0])) )
+    print("#Transportation POI = %d " % (len(dfpoi[dfpoi['roleID'] == 1])) )
+    print("#Residence POI = %d " % (len(dfpoi[dfpoi['roleID'] == 2])) )
+    print("#Edu POI = %d " % (len(dfpoi[dfpoi['roleID'] == 3])) )
+    print("#Work POI = %d " % (len(dfpoi[dfpoi['roleID'] == 4])) )
+    print("#Entertainment POI = %d " % (len(dfpoi[dfpoi['roleID'] == 5])) )
+
     
 def assignRole2Cell(dfCellLoc, dfPOI):
     '''
@@ -96,10 +111,60 @@ def assignRole2Cell(dfCellLoc, dfPOI):
                 nCellRole = tp[0]
         lsCellRole.append(nCellRole)
     
-    dfCellLoc['cellRole'] = lsCellRole
+    dfCellLoc['roleID'] = lsCellRole
+    
+    return dfCellLoc
+
+def roleAssignmentCallback(rt):
+    '''
+        merge the assigned cell together
+    '''
+    global g_dfCellLocRole
+
+    # concatenate
+    g_dfCellLocRole = pd.concat([g_dfCellLocRole, rt])
+    
+    print("==>%d cells have been processed, Progress:%.2f" % \
+    (len(g_dfCellLocRole), len(g_dfCellLocRole)*1.0/g_nCellNum) )
+    
+def assignRoleInParallel(dfCellLoc, dfPOI, strOutPath):
+    '''
+        start multiple processes to assign role in parallel
+    '''
+    global g_nCellNum
+    g_nCellNum= len(dfCellLoc)
+    
+    nCellNum = len(dfCellLoc)
+    nPoolSize = MAX_PROC_NUM
+    nCellPerProc = min( int(nCellNum/nPoolSize), MAX_CELL_PER_PROC)
+    
+    pool = multiprocessing.Pool(processes=nPoolSize)
+    nStartIndex = 0
+    while nStartIndex < nCellNum:
+        nEndIndex = min(nStartIndex+nCellPerProc, nCellNum)
+        pool.apply_async(assignRole2Cell, args=(dfCellLoc[nStartIndex:nEndIndex], dfPOI), callback = roleAssignmentCallback)
+        nStartIndex = nEndIndex
+    pool.close()
+    pool.join()
+    
+    g_dfCellLocRole.to_csv(strOutPath)
+    
+    
 
 if __name__ == '__main__':
-    dfCellLoc = pd.read_csv("d:\\playground\\cell_loc_filled.csv", index_col='lac-cid')
-    dfPoi = pd.read_csv("d:\\playground\\weibo_poi_role_gz.txt", index_col='_id')
-    assignRole2Cell(dfCellLoc, dfPoi)
-    dfCellLoc.to_csv("d:\\playground\\cell_loc_role.txt")
+
+    strPoiRolePath = "../../data/weibo_poi_role_gz.txt"
+    strPoiPath = "../../data/weibo_poi_gz.txt"
+    strCellLocFilled = "../../data/cell_loc_filled.txt"
+    strCellLocRole = "../../data/cell_loc_role.txt"
+
+    # generate poi_role_dict
+    print("start to generate poi role...")
+    generatePoiRole(strPoiPath, strPoiRolePath)
+    
+    #generate cell_loc_role
+    print("start to generate poi role...")
+    dfCellLoc = pd.read_csv(strCellLocFilled, index_col='lac-cid')
+    dfPoi = pd.read_csv(strPoiRolePath, index_col='_id')
+    assignRoleInParallel(dfCellLoc, dfPoi[dfPoi['roleID'] != 0], strCellLocRole)
+    print("assignRoleInParallel is finished.")
