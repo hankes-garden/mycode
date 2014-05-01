39f732e7b6bd5f15224ac57e395b2969673c4c53
jason@think <hankes_garden@163.com>
2014-05-01 21:59:38 +0800

    1. change weight;

--- python_lab/pylab/region_type.py
+++ python_lab/pylab/region_type.py
@@ -39,11 +39,11 @@ g_dcRegionTypeName = {ID_TYPE_UNKNOWN: "unknown",\
                   ID_TYPE_ENTERTAINMENT: "entertainment"}
 
 g_dcRegionWeight = {ID_TYPE_UNKNOWN: 0.003226,\
-                  ID_TYPE_TRANSPORTATION: 0.494150,\
+                  ID_TYPE_TRANSPORTATION: 0.394150,\
                   ID_TYPE_RESIDENCE: 0.400324,\
                   ID_TYPE_EDU: 0.056779,\
                   ID_TYPE_WORK: 0.044356,\
-                  ID_TYPE_ENTERTAINMENT: 0.001165}
+                  ID_TYPE_ENTERTAINMENT: 0.011165}
 
 DEFAULT_REGION_COVERAGE = 2000 # unit = meter
 
