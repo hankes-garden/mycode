8c3071967c92ab7fc2695ad246d1be6a2ec77969
jason@think <hankes_garden@163.com>
2014-05-06 15:58:49 +0800

    1. refactoring;

--- python_lab/pylab/app_usage.py
+++ python_lab/pylab/app_usage.py
@@ -31,44 +31,10 @@ def getCategoryTraffic(dfAgg):
     '''
     nTotalTraffic = dfAgg.sum().sum()
     dcAppCategoryTraffic = {}
-    dcAppCategoryTraffic['web_browsing'] = \
-    dfAgg.loc[dfAgg.index.isin(app_category.g_lsWebBrowsing)].sum(axis=1).sum()/nTotalTraffic
     
-    dcAppCategoryTraffic['p2p'] = \
-    dfAgg.loc[dfAgg.index.isin(app_category.g_lsP2P)].sum(axis=1).sum()/nTotalTraffic
-    
-    dcAppCategoryTraffic['im'] = \
-    dfAgg.loc[dfAgg.index.isin(app_category.g_lsIM)].sum(axis=1).sum()/nTotalTraffic
-    
-    dcAppCategoryTraffic['reading'] = \
-    dfAgg.loc[dfAgg.index.isin(app_category.g_lsReading)].sum(axis=1).sum()/nTotalTraffic
-    
-    dcAppCategoryTraffic['social_network'] \
-    = dfAgg.loc[dfAgg.index.isin(app_category.g_lsSNS)].sum(axis=1).sum()/nTotalTraffic
-  
-    dcAppCategoryTraffic['video'] = \
-    dfAgg.loc[dfAgg.index.isin(app_category.g_lsVideo)].sum(axis=1).sum()/nTotalTraffic
-    
-    dcAppCategoryTraffic['music'] = \
-    dfAgg.loc[dfAgg.index.isin(app_category.g_lsMusic)].sum(axis=1).sum()/nTotalTraffic
-    
-    dcAppCategoryTraffic['app_market'] = \
-    dfAgg.loc[dfAgg.index.isin(app_category.g_lsAppMarket)].sum(axis=1).sum()/nTotalTraffic
-    
-    dcAppCategoryTraffic['game'] = \
-    dfAgg.loc[dfAgg.index.isin(app_category.g_lsGame)].sum(axis=1).sum()/nTotalTraffic
-    
-    dcAppCategoryTraffic['email'] = \
-    dfAgg.loc[dfAgg.index.isin(app_category.g_lsEmail)].sum(axis=1).sum()/nTotalTraffic
-    
-    dcAppCategoryTraffic['stock'] = \
-    dfAgg.loc[dfAgg.index.isin(app_category.g_lsStock)].sum(axis=1).sum()/nTotalTraffic
-    
-    dcAppCategoryTraffic['shopping'] = \
-    dfAgg.loc[dfAgg.index.isin(app_category.g_lsShopping)].sum(axis=1).sum()/nTotalTraffic
-    
-    dcAppCategoryTraffic['map'] = \
-    dfAgg.loc[dfAgg.index.isin(app_category.g_lsMap)].sum(axis=1).sum()/nTotalTraffic
+    for tp in app_category.g_dcCategory.items():
+        dcAppCategoryTraffic[tp[0]] = \
+            dfAgg.loc[dfAgg.index.isin(tp[1])].sum(axis=1).sum()/nTotalTraffic
     
     sAppCategoryTraffic = pd.Series(dcAppCategoryTraffic)
     return sAppCategoryTraffic
